96bbd02bec4e54fd533be86f36d282795eb2e4bf
dj <dj@ime.usp.br>
2022-02-09 10:05:51 -0300

    Criando as pastas MVVM

--- app/src/main/java/com/oceanbrasil/ocean_android_room_fev_2022/view/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_fev_2022/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.oceanbrasil.ocean_android_room_fev_2022
+package com.oceanbrasil.ocean_android_room_fev_2022.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oceanbrasil.ocean_android_room_fev_2022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
